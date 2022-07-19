505b80cf628e3adb967cb89c51df803ccb3b584f
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2022-07-19 21:19:23 +0200

    Update scores.js

--- script/scores.js
+++ script/scores.js
@@ -3,7 +3,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 
-async function readScores(array, p=null) {
+function readScores(array, p=null) {
     
     if (p==null) {
         const {sc, error} = await _supabase
