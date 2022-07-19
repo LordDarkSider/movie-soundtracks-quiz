7d23a58a2ebf5c03c571182c32d94fc2c5c8aa5f
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2022-07-19 19:04:37 +0200

    Update scores.js

--- script/scores.js
+++ script/scores.js
@@ -1,10 +1,10 @@
 const SUPABASE_URL = 'https://hyebhkfmkbvvpblhrrob.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5ZWJoa2Zta2J2dnBibGhycm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgyMzc2MzksImV4cCI6MTk3MzgxMzYzOX0.x9UmvjtQsuS5m8dY7ynHKKxnapKxtzMMVTqnrReFviY'
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 
 async function readScores(array) {
-    const {scores, error} = await supabase
+    const {scores, error} = await _supabase
         .from(array)
         .select()
     return scores
@@ -12,7 +12,7 @@ async function readScores(array) {
 
 
 async function insertScore(array, p, s) {
-    const {scores, error} = await supabase
+    const {scores, error} = await _supabase
         .from(array)
         .upsert({ pseudo: p, score: s})
 
