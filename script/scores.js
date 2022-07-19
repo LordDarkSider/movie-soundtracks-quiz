7a05a06d9847718ff55564ec9ce117ac53572301
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2022-07-19 16:20:49 +0200

    Create scores.js

---
+++ script/scores.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://hyebhkfmkbvvpblhrrob.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5ZWJoa2Zta2J2dnBibGhycm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgyMzc2MzksImV4cCI6MTk3MzgxMzYzOX0.x9UmvjtQsuS5m8dY7ynHKKxnapKxtzMMVTqnrReFviY'
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
