9e6a583d5d7a41a214fe646c98f0a98b8e33c601
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2022-07-19 17:30:12 +0200

    Update scores.js

--- script/scores.js
+++ script/scores.js
@@ -2,12 +2,14 @@ const SUPABASE_URL = 'https://hyebhkfmkbvvpblhrrob.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5ZWJoa2Zta2J2dnBibGhycm9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgyMzc2MzksImV4cCI6MTk3MzgxMzYzOX0.x9UmvjtQsuS5m8dY7ynHKKxnapKxtzMMVTqnrReFviY'
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-async function load_scores() {
-    const { scores, error } = await _supabase
-            .from('Normal Ranking')
-            .select()
+async function insertData() {
+    const { sc , error } = await _supabase
+        .from('Normal Ranking')
+          .insert([
+            { name: 'Léon', score: 777}
+        ])
 
-    console.log(scores)
+    console.log(sc)
     console.log(error)
 }
-load_scores()
+insertData()
